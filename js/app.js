f286b471eabfba89974048a78e2c0c68d5e9a9a4
Martin Dos Santos <martinplateado@gmail.com>
2015-11-27 02:02:13 -0300

    Tweaks navbar
    Mejora de practicas y agregado de efecto hover sobre la navbar.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,1 @@
 $(document).foundation();
-$('li').mouseover(function(e)
-{
-    e.stopPropagation();
-    $(this).addClass('lihover');
-});
-
-$('li').mouseout(function()
-{
-    $(this).removeClass('lihover');
-});
