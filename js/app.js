d903a7d9a46a1dbafd3b6247665255822c586934
Martin Dos Santos <martinplateado@gmail.com>
2015-11-28 04:25:13 -0300

    Efecto check en respuestas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+$("a.boton").hover(
+    function () {
+        $(this).html(function (i, origText) {
+            return "<i class='fi-check'>&#xf126;</i>" + origText;
+        });
+    },
+    function () {
+        $(this).find("i").remove();
+    }
+);
